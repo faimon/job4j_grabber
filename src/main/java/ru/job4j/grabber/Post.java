c45d4158f7dc9a9c27703fd7c4dbfaf6791339b4
andreykatanov <faimon96@gmail.com>
2020-09-08 21:34:18 +0700

    2.2. Модель данных - Post. [#337990]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,42 @@
+package ru.job4j.grabber;
+
+import java.util.Calendar;
+
+public class Post {
+    private String topic;
+    private String description;
+    private String url;
+    private Calendar createDate;
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Calendar getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Calendar createDate) {
+        this.createDate = createDate;
+    }
+}
