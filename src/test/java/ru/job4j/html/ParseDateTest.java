48328319feabedfada91cc20db84884be7038e07
andreykatanov <faimon96@gmail.com>
2020-09-10 10:05:40 +0700

    update for travis tests #TRY № 3

--- src/test/java/ru/job4j/html/ParseDateTest.java
+++ src/test/java/ru/job4j/html/ParseDateTest.java
@@ -13,15 +13,38 @@ public class ParseDateTest {
     public void whenOneDate() {
         String line = "1 сен 16, 10:56";
         Calendar date = ParseDate.convertToCalendarFormat(line);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
-        assertThat(simpleDateFormat.format(date.getTime()), is("01/09/16, 10:56"));
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("d/MM/yy, k:mm");
+        assertThat(simpleDateFormat.format(date.getTime()), is("1/09/16, 10:56"));
     }
 
     @Test
     public void whenOtherDate() {
         String line = "23 июл 20, 20:55";
         Calendar date = ParseDate.convertToCalendarFormat(line);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("d/MM/yy, k:mm");
         assertThat(simpleDateFormat.format(date.getTime()), is("23/07/20, 20:55"));
+    }
+
+    @Test
+    public void whenOtherDate2() {
+        String line = "сегодня, 10:56";
+        Calendar date = ParseDate.convertToCalendarFormat(line);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("d/MM/yy, k:mm");
+        Calendar currentDate = Calendar.getInstance();
+        currentDate.set(Calendar.HOUR_OF_DAY, 10);
+        currentDate.set(Calendar.MINUTE, 56);
+        assertThat(simpleDateFormat.format(date.getTime()), is(simpleDateFormat.format(currentDate.getTime())));
+    }
+
+    @Test
+    public void whenOtherDate3() {
+        String line = "вчера, 10:56";
+        Calendar date = ParseDate.convertToCalendarFormat(line);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("d/MM/yy, k:mm");
+        Calendar currentDate = Calendar.getInstance();
+        currentDate.set(Calendar.DAY_OF_MONTH, currentDate.get(Calendar.DAY_OF_MONTH) - 1);
+        currentDate.set(Calendar.HOUR_OF_DAY, 10);
+        currentDate.set(Calendar.MINUTE, 56);
+        assertThat(simpleDateFormat.format(date.getTime()), is(simpleDateFormat.format(currentDate.getTime())));
     }
 }
