4cf5e787d4e4fb0156ea5bd10553aef4f75b27e1
andreykatanov <faimon96@gmail.com>
2020-09-11 11:15:55 +0700

    7. Web. [#337995]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -11,6 +11,8 @@ import ru.job4j.store.PsqlStore;
 import ru.job4j.store.Store;
 
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -74,6 +76,26 @@ public class Grabber implements Grab {
         }
     }
 
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes("UTF-16"));
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
     public static void main(String[] args) throws Exception {
         Grabber grab = new Grabber();
         grab.cfg();
@@ -81,5 +103,6 @@ public class Grabber implements Grab {
         Store store = grab.store();
         PropertyConfigurator.configure("src/main/resources/log4j.properties");
         grab.init(new SqlRuParse(), store, scheduler);
+        grab.web(store);
     }
 }
